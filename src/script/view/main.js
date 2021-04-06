51a8c639633b767bfbab50e11ba7b3f2500e488b
irfan-fauzi <irfanwintech@yahoo.com>
2021-04-06 11:08:41 +0700

    coba

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -4,53 +4,47 @@ import '../view/component/club-list.js';
 
 const main = () => {
 
+  // seleksi custom element
   const searchElement = document.querySelector("search-bar");
-  
+  // seleksi bukan custom element
   const clubListElement = document.querySelector("#clubList");
-
+  
+  // aksi ketika tombol search di klik :
+  
   const onButtonSearchClicked = async () => {
-
     try {
-      
+       // mengambil data secara async => ke DataSource (proses pencarian data pakai Promise)
       const result = await DataSource.searchClub(searchElement.value);
       renderResult(result);
 
     } catch(message){
-
       fallbackResult(message)
     }
-   
       
   };
 
+  // fungsi ketika DataSOurce menghasilkan data pencarian 
   const renderResult = results => {
-    // clubListElement.innerHTML = "";
-    // results.forEach(club => {
-
-    //   const { name, fanArt, description } = club
-
-    //   const clubElement = document.createElement("div");
-    //   clubElement.setAttribute("class", "club");
-
-    //   clubElement.innerHTML = `<img class="fan-art-club" src=${fanArt} alt="Fan Art">
-    //     <div class="club-info">
-    //     <h2>${name}</h2>
-    //     <p>${description}</p>
-    //     </div>`;
-    //   clubListElement.appendChild(clubElement);
-    // })
+    // clubListElemen di isi kosong supaya tidak createElement terus menerus
     clubListElement.innerHTML = "";
+
+    // membuat element clubList yang sudah didefinisikan di class ClubList
     const clubList = document.createElement('club-list');
+    // class ClubList memanggil method set dataClub yang di inisialisasi dan di isi oleh result (data pencarian)
     clubList.dataClub = results;
+    // menaruh element clubList di dalam clubListElement
     clubListElement.appendChild(clubList);
   };
 
+  // fungsi ketika promise reject / data pencarian tida ditemukan
   const fallbackResult = message => {
     clubListElement.innerHTML = "";
     clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`
   };
 
   // buttonSearchElement.addEventListener("click", onButtonSearchClicked);
+
+  // di taruh dibawah
   searchElement.clickEvent = onButtonSearchClicked;
   
 };
