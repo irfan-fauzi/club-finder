b8b81b010526492932b7de6e310f2b2e947c47fc
irfan-fauzi <irfanwintech@yahoo.com>
2021-04-08 11:20:15 +0700

    shadow dom berhasil dimengerti

--- src/script/view/component/club-item.js
+++ src/script/view/component/club-item.js
@@ -1,5 +1,10 @@
 class ClubItem extends HTMLElement{
 
+  constructor(){
+    super();
+    this.shadowDom = this.attachShadow({mode: "open"});
+  }
+
   set dataClub(data){
     this._dataclubs = data;
     this.render();
@@ -7,7 +12,42 @@ class ClubItem extends HTMLElement{
   
   render(){
     
-    this.innerHTML = `
+    this.shadowDom.innerHTML = `
+    <style>
+    :host {
+      display: block;
+      margin-bottom: 18px;
+      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+      border-radius: 10px;
+      overflow: hidden;
+    }
+    
+    .fan-art-club {
+      width: 100%;
+      max-height: 300px;
+      object-fit: cover;
+      object-position: center;
+    }
+    
+    .club-info {
+      padding: 24px;
+    }
+    
+    .club-info > h2 {
+      font-weight: lighter;
+    }
+    
+    .club-info > p {
+      margin-top: 10px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 10; /* number of lines to show */
+    }
+
+    </style>
+
     <img class="fan-art-club" src=${this._dataclubs.fanArt} alt="Fan Art">
     <div class="club-info">
       <h2>${this._dataclubs.name}</h2>
--- src/script/view/component/club-list.js
+++ src/script/view/component/club-list.js
@@ -2,10 +2,10 @@ import './club-item.js';
 
 class ClubList extends HTMLElement{
 
-  // constructor(){
-  //   super();
-  //   this._shadowRoot = this.attachShadow({mode: "open"});
-  // }
+  constructor(){
+    super();
+    this.shadowRoot = this.attachShadow({mode: "open"});
+  }
 
   set dataClub(data){
     this._dataClub = data;
@@ -13,22 +13,33 @@ class ClubList extends HTMLElement{
   }
 
   renderError(message){
-    this.innerHTML = "";
-    this.innerHTML += `
-    <h2 class="placeholder">${message}</h2>
-    `
+
+    this.shadowRoot.innerHTML = `
+    <style>
+
+      .placeholder {
+      font-weight: lighter;
+      color: rgba(0, 0, 0, 0.5);
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+    }
+    </style>
+    <h2 class="placeholder">${message}</h2>`;
+    
   }
   
   render(){  
     
-    this.innerHTML = "";
+    this.shadowRoot.innerHTML = "";
     this._dataClub.forEach(data => {
 
       const clubElement = document.createElement("club-item");
       clubElement.setAttribute("class", "club");
 
       clubElement.dataClub = data;
-      this.appendChild(clubElement)
+      this.shadowRoot.appendChild(clubElement)
 
     })
   }
